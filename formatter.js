ef109d75e15c351d88d0f39fe0fa5a274d8acc06
haiyang5210 <haiyang5210@gmail.com>
2025-03-23 15:39:17 -0400

    Signed-off-by: haiyang5210 <haiyang5210@gmail.com>

--- formatter.js
+++ formatter.js
@@ -109,7 +109,7 @@ function objectToHTML(json) {
   const length = keys.length
 
   let output =
-      '<div class="collapser"></div>{<span class="ellipsis"></span><ul class="obj collapsible">',
+      '<div class="collapser"><i><svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></i></div>{<span class="ellipsis"></span><ul class="obj collapsible">',
     hasContents = false,
     index = 0
 
