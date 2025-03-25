a3019aff95b42fe68caa492badb71bc500c92de7
haiyang5210 <haiyang5210@gmail.com>
2025-03-25 13:05:15 -0400

    Signed-off-by: haiyang5210 <haiyang5210@gmail.com>

--- formatter.js
+++ formatter.js
@@ -4,6 +4,13 @@
  * Original author: Benjamin Hollis
  * Maintained by teocci on 11/28/17.
  */
+function parseJSONValueType(value) {
+  if (value === undefined) return 'undefined'
+  if (value === null) return 'null'
+  if (Array.isArray(value)) return 'array'
+  if (typeof value === 'object') return 'object'
+  return typeof value
+}
 function htmlEncode(t) {
   if (t == null) return ''
 
@@ -20,63 +27,66 @@ function addComma(index, length) {
   return index < length - 1 ? ',' : ''
 }
 
-function decorateWithSpan(value, className, index, length) {
+function decorateWithSpan(value, index, length, prefix) {
   // return `<span class="${className}">${htmlEncode(value)}</span>`
-  const types = ['number','string','boolean','undefined','symbol','bigint','null']
+  let str = ''
+  const type = parseJSONValueType(value)
+  switch (type) {
+    case 'null':
+      str = `<input type="text" disabled value="null">`
+      break
+    case 'undefined':
+      str = `<input type="text" disabled value="undefined">`
+      break
+    case 'boolean':
+      str = `<select name="value" idx="${prefix}:value">
+      <option value="true" ${value ? 'selected' : ''}>true</option>
+      <option value="false" ${!value ? 'selected' : ''}>false</option></select>`
+      break
+    case 'number':
+      str = `<input type="number" idx="${prefix}:value" value="${value}">`
+      break
+    case 'string':
+      str = `<input type="text" idx="${prefix}:value" value="${value}">`
+      break
+    default:
+      str = ''
+  }
   return `<div class="type-select-item">
-      <select name="type" class="type-select">
-        <option value="string" ${className === 'string' ? 'selected' : ''}>string</option>
+      <select name="type" idx="${prefix}:type" class="type-select">
+        <option value="string" ${type === 'string' ? 'selected' : ''}>string</option>
         <option value="object">object</option>
         <option value="array">array</option>
-        <option value="number" ${className === 'number' ? 'selected' : ''}>number</option>
-        <option value="boolean" ${className === 'boolean' ? 'selected' : ''}>boolean</option>
-        <option value="undefined" ${className === 'undefined' ? 'selected' : ''}>undefined</option>
-        <option value="symbol" ${className === 'symbol' ? 'selected' : ''}>symbol</option>
-        <option value="bigint" ${className === 'bigint' ? 'selected' : ''}>bigint</option>
-        <option value="null" ${className === 'null' ? 'selected' : ''}>null</option>
+        <option value="number" ${type === 'number' ? 'selected' : ''}>number</option>
+        <option value="boolean" ${type === 'boolean' ? 'selected' : ''}>boolean</option>
+        <option value="undefined" ${type === 'undefined' ? 'selected' : ''}>undefined</option>
+        <option value="null" ${type === 'null' ? 'selected' : ''}>null</option>
       </select>
     </div><div class="value-input-item">
-      <input type="text" value="${htmlEncode(value)}">
+      ${str}
     </div>${addComma(index, length)}<div class="operate-item">
       
     </div>`
 }
 
-function valueToHTML(value, index, length) {
+function valueToHTML(value, index, length, prefix) {
   const type = value === null ? 'null' : typeof value
   let output = ''
 
   switch (type) {
     case 'object':
-      output += value && value.constructor === Array ? arrayToHTML(value) : objectToHTML(value)
+      output +=
+        value && value.constructor === Array
+          ? arrayToHTML(value, prefix)
+          : objectToHTML(value, prefix)
       break
 
     case 'number':
-      output += decorateWithSpan(value, 'number', index, length)
-      break
-
     case 'string':
-      output += decorateWithSpan(`"${value}"`, 'string', index, length)
-      break
-
     case 'boolean':
-      output += decorateWithSpan(value, 'boolean', index, length)
-      break
-
     case 'undefined':
-      output += decorateWithSpan('undefined', 'undefined', index, length)
-      break
-
-    case 'symbol':
-      output += decorateWithSpan(value.toString(), 'symbol', index, length)
-      break
-
-    case 'bigint':
-      output += decorateWithSpan(value.toString(), 'bigint', index, length)
-      break
-
     case 'null':
-      output += decorateWithSpan('null', 'null', index, length)
+      output += decorateWithSpan(value, index, length, prefix)
       break
 
     default:
@@ -87,95 +97,98 @@ function valueToHTML(value, index, length) {
   return output
 }
 
-function arrayToHTML(json) {
-  const length = json.length
-  let output =
-      `<div class="collapser"><i><svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></i></div>
-      <select name="type" class="type-select">
+function arrayToHTML(data, prefix) {
+  const length = data.length
+  let output = `<div class="collapser"><i><svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></i></div>
+      <select name="type" idx="${prefix}:type" style="${
+      prefix ? '' : 'display:none'
+    }" class="type-select">
         <option value="string">string</option>
         <option value="object">object</option>
         <option value="array" selected>array</option>
         <option value="number">number</option>
         <option value="boolean">boolean</option>
         <option value="undefined">undefined</option>
-        <option value="symbol">symbol</option>
-        <option value="bigint">bigint</option>
         <option value="null">null</option>
-      </select>[ <span class="ellipsis"></span>
-      <div class="operate-item">
-        <span role="img" class="copy-body" tabindex="-1">复制数组</span>
-        <span role="img" class="paste-body" tabindex="-1">粘贴数组</span>
+      </select>[<div class="operate-item" idx="${prefix}:value" style="${prefix ? '' : 'display:none'}">
+        <span class="cmd copy-body" tabindex="-1">复制数组</span>
+        <span class="cmd paste-body" tabindex="-1">粘贴数组</span>
       </div>
+      <span class="ellipsis"></span>
       <ul class="array collapsible">`,
-    hasContents = false,
-    index = 0
+    hasContents = false
 
-  let vline = '<div class="vline"><svg viewBox="64 64 896 896" focusable="false" data-icon="holder" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M300 276.5a56 56 0 1056-97 56 56 0 00-56 97zm0 284a56 56 0 1056-97 56 56 0 00-56 97zM640 228a56 56 0 10112 0 56 56 0 00-112 0zm0 284a56 56 0 10112 0 56 56 0 00-112 0zM300 844.5a56 56 0 1056-97 56 56 0 00-56 97zM640 796a56 56 0 10112 0 56 56 0 00-112 0z"></path></svg></div>' 
-  for (const item of json) {
+  let vline =
+    '<div class="vline"><svg viewBox="64 64 896 896" focusable="false" data-icon="holder" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M300 276.5a56 56 0 1056-97 56 56 0 00-56 97zm0 284a56 56 0 1056-97 56 56 0 00-56 97zM640 228a56 56 0 10112 0 56 56 0 00-112 0zm0 284a56 56 0 10112 0 56 56 0 00-112 0zM300 844.5a56 56 0 1056-97 56 56 0 00-56 97zM640 796a56 56 0 10112 0 56 56 0 00-112 0z"></path></svg></div>'
+  data.forEach((item, index) => {
     hasContents = true
-    output += `<li><div class="hoverable">${vline}${valueToHTML(item, index, length)}</div></li>`
-    index++
-  }
+    output += `<li><div class="hoverable">${vline}${valueToHTML(
+      item,
+      index,
+      length,
+      prefix + '-' + index,
+    )}</div></li>`
+  })
   output += '</ul>]'
 
-  if (!hasContents) return '[ ]'
+  // if (!hasContents) return '[ ]'
 
   return output
 }
 
 /*
       <div class="operate-item">
-        <span role="img" class="add-child" tabindex="-1">新增子项</span>
-        <span role="img" class="copy-keyvalue" tabindex="-1">复制(键/值)</span>
-        <span role="img" class="copy-body" tabindex="-1">复制(数组/对象)</span>
-        <span role="img" class="clear-content" tabindex="-1">清空(数组/对象)</span>
-        <span role="img" class="add-before" tabindex="-1">新增(在前)</span>
-        <span role="img" class="delete" tabindex="-1">删除</span>
+        <span role="img" class="cmd add-child" tabindex="-1">新增子项</span>
+        <span role="img" class="cmd copy-keyvalue" tabindex="-1">复制(键/值)</span>
+        <span role="img" class="cmd copy-body" tabindex="-1">复制(数组/对象)</span>
+        <span role="img" class="cmd clear-content" tabindex="-1">清空(数组/对象)</span>
+        <span role="img" class="cmd add-before" tabindex="-1">新增(在前)</span>
+        <span role="img" class="cmd delete" tabindex="-1">删除</span>
       </div>
 */
-function objectToHTML(json) {
-  const keys = Object.keys(json)
+function objectToHTML(data, prefix) {
+  const arr = Object.entries(data)
+  const keys = Object.keys(data)
   const length = keys.length
 
-  let output =
-      `<div class="collapser"><i><svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></i></div>
-      {<select name="type" class="type-select">
+  let output = `<div class="collapser"><i><svg viewBox="0 0 1024 1024" focusable="false" data-icon="caret-down" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg></i></div>
+      {<select name="type" idx="${prefix}:type" style="${
+      prefix ? '' : 'display:none'
+    }" class="type-select">
         <option value="string">string</option>
         <option value="object" selected>object</option>
         <option value="array">array</option>
         <option value="number">number</option>
         <option value="boolean">boolean</option>
         <option value="undefined">undefined</option>
-        <option value="symbol">symbol</option>
-        <option value="bigint">bigint</option>
         <option value="null">null</option>
       </select>
-      <span class="ellipsis"></span>
       <div class="operate-item">
       </div>
+      <span class="ellipsis"></span>
       <ul class="obj collapsible">`,
-    hasContents = false,
-    index = 0
+    hasContents = false
 
-  let vline = '<div class="vline"><svg viewBox="64 64 896 896" focusable="false" data-icon="holder" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M300 276.5a56 56 0 1056-97 56 56 0 00-56 97zm0 284a56 56 0 1056-97 56 56 0 00-56 97zM640 228a56 56 0 10112 0 56 56 0 00-112 0zm0 284a56 56 0 10112 0 56 56 0 00-112 0zM300 844.5a56 56 0 1056-97 56 56 0 00-56 97zM640 796a56 56 0 10112 0 56 56 0 00-112 0z"></path></svg></div>' 
-  for (const key of keys) {
+  let vline =
+    '<div class="vline"><svg viewBox="64 64 896 896" focusable="false" data-icon="holder" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M300 276.5a56 56 0 1056-97 56 56 0 00-56 97zm0 284a56 56 0 1056-97 56 56 0 00-56 97zM640 228a56 56 0 10112 0 56 56 0 00-112 0zm0 284a56 56 0 10112 0 56 56 0 00-112 0zM300 844.5a56 56 0 1056-97 56 56 0 00-56 97zM640 796a56 56 0 10112 0 56 56 0 00-112 0z"></path></svg></div>'
+  arr.forEach(([key, value], index) => {
+    let idx = prefix + '-' + index
     hasContents = true
     output += `<li><div class="hoverable">${vline}`
-    output += `<span class="property"><input type="text" value="${htmlEncode(
+    output += `<span class="property"><input type="text" idx="${idx}:key" value="${htmlEncode(
       key,
-    )}"></span>: ${valueToHTML(json[key], index, length)}`
+    )}"></span>: ${valueToHTML(value, index, length, idx)}`
     output += '</div></li>'
-    index++
-  }
+  })
   output += '</ul>}'
   if (!hasContents) return '{ }'
 
   return output
 }
 
-function jsonToHTML(json, fnName) {
+function jsonToHTML(data, fnName) {
   let output = fnName ? `<div class="callback-function">${fnName}(</div>` : ''
-  output += `<div id="json">${valueToHTML(json)}</div>`
+  output += `<div id="json6">${valueToHTML(data, null, null, '')}</div>`
 
   return `${output}${fnName ? '<div class="callback-function">)</div>' : ''}`
 }
--- jsonbyxpath.js
+++ jsonbyxpath.js
@@ -7,13 +7,36 @@ function parseJSONValueType(value) {
   return typeof value
 }
 
+function transValueType(value, type) {
+  if (parseJSONValueType(value) === type) return value
+  let result = value
+  if (type === 'null') result = null
+  if (type === 'undefined') result = undefined
+  if (type === 'array') result = []
+  if (type === 'object') result = {}
+  if (type === 'string') {
+    result = [null, undefined].includes(value) ? '' : String(value)
+  }
+  if (type === 'number') {
+    result = Number(value)
+    if (Number.isNaN(result)) {
+      result = 0
+    }
+  }
+  if (type === 'boolean') {
+    result = value === 'false' ? false : Boolean(value)
+  }
+  return result
+}
+
 function findValueByXPath(obj, path) {
   if (!path || !path.replace(/\s+/g, '')) throw new Error('path is required.')
   if (!obj) throw new Error('obj is required.')
+  path = path.replace(/^-/, '')
 
-  Object.entries(obj).forEach(([key, value]) => {
-    console.log(key, value)
-  })
+  // Object.entries(obj).forEach(([key, value]) => {
+  //   console.log(key, value)
+  // })
   path = path.replace(/\s+/g, '')
   let dep = path.split(':')[0].split('-')
   let key = path.split(':')[1]
@@ -44,10 +67,11 @@ var getValueByXPath = findValueByXPath
 function setValueByXPath(obj, path, val) {
   if (!path || !path.replace(/\s+/g, '')) throw new Error('path is required.')
   if (!obj) throw new Error('obj is required.')
+  path = path.replace(/^-/, '')
 
-  Object.entries(obj).forEach(([key, value]) => {
-    console.log(key, value)
-  })
+  // Object.entries(obj).forEach(([key, value]) => {
+  //   console.log(key, value)
+  // })
   path = path.replace(/\s+/g, '')
   let dep = path.split(':')[0].split('-')
   let key = path.split(':')[1]
@@ -78,34 +102,11 @@ function setValueByXPath(obj, path, val) {
     result = getValueByXPath(obj, path.split(':')[0] + ':key')
   }
   if (key === 'value') {
-    target[kv[0]] = val
+    target[kv[0]] = transValueType(val, parseJSONValueType(kv[1]))
     result = getValueByXPath(obj, path.split(':')[0] + ':value')
   }
   if (key === 'type') {
-    if (val === 'null') {
-      target[kv[0]] = null
-    }
-    if (val === 'undefined') {
-      target[kv[0]] = undefined
-    }
-    if (val === 'array') {
-      target[kv[0]] = []
-    }
-    if (val === 'object') {
-      target[kv[0]] = {}
-    }
-    if (val === 'string') {
-      target[kv[0]] = String(kv[1])
-    }
-    if (val === 'number') {
-      target[kv[0]] = Number(kv[1])
-      if (Number.isNaN(target[kv[0]])) {
-        target[kv[0]] = 0
-      }
-    }
-    if (val === 'boolean') {
-      target[kv[0]] = Boolean(kv[1])
-    }
+    target[kv[0]] = transValueType(kv[1], val)
     result = getValueByXPath(obj, path.split(':')[0] + ':value')
   }
   return result
